6857308f12296434a8ce197592bfb4d616653096
RadGrabowski <radek18g@gmail.com>
2020-05-07 19:22:11 +0200

    added pdf render of each order on the admin page

--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import get_object_or_404
+from .utils import render_to_pdf
 from .models import OrderItem, Order
 from .forms import OrderCreateForm
 from cart.cart import Cart
@@ -30,3 +32,16 @@ def order_create(request):
 def admin_order_detail(request, order_id):
     order = get_object_or_404(Order, id=order_id)
     return render(request, 'admin/orders/order/detail.html', {'order': order})
+
+
+@staff_member_required
+def admin_order_pdf(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+
+    pdf = render_to_pdf('admin/orders/order/pdf.html', {'order': order})
+    if pdf:
+        response = HttpResponse(pdf, content_type='application/pdf')
+        response['Content-Disposition'] = f'filename=order_{order.id}.pdf'
+        return response
+    return HttpResponse("Not found")
+
